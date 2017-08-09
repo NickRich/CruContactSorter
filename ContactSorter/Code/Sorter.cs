b17803601e688a14e09fa70c762036e9817fcdfd
Nicholas Austin Richardson <richa271@purdue.edu>
2017-08-09 09:06:28 -0400

    Add project files.

---
+++ ContactSorter/Code/Sorter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactSorter
+{
+    class Sorter
+    {
+        #region ---Global Variables---
+        private LinkedList<Contact> caryE = new LinkedList<Contact>();
+        private LinkedList<Contact> caryNE = new LinkedList<Contact>();
+        private LinkedList<Contact> caryNW = new LinkedList<Contact>();
+        private LinkedList<Contact> carySE = new LinkedList<Contact>();
+        private LinkedList<Contact> carySW = new LinkedList<Contact>();
+        private LinkedList<Contact> caryW = new LinkedList<Contact>();
+        private LinkedList<Contact> earhartM = new LinkedList<Contact>();
+        private LinkedList<Contact> earhartW = new LinkedList<Contact>();
+        private LinkedList<Contact> firstStreetCentralM = new LinkedList<Contact>();
+        private LinkedList<Contact> firstStreetCentralW = new LinkedList<Contact>();
+        private LinkedList<Contact> firstStreetEastM = new LinkedList<Contact>();
+        private LinkedList<Contact> firstStreetEastW = new LinkedList<Contact>();
+        private LinkedList<Contact> firstStreetWestM = new LinkedList<Contact>();
+        private LinkedList<Contact> firstStreetWestW = new LinkedList<Contact>();
+        private LinkedList<Contact> harrisonM = new LinkedList<Contact>();
+        private LinkedList<Contact> harrisonW = new LinkedList<Contact>();
+        private LinkedList<Contact> hawkinsM = new LinkedList<Contact>();
+        private LinkedList<Contact> hawkinsW = new LinkedList<Contact>();
+        private LinkedList<Contact> hillenbrandM = new LinkedList<Contact>();
+        private LinkedList<Contact> hillenbrandW = new LinkedList<Contact>();
+        private LinkedList<Contact> hilltopM = new LinkedList<Contact>();
+        private LinkedList<Contact> hilltopW = new LinkedList<Contact>();
+        private LinkedList<Contact> honorsNorthM = new LinkedList<Contact>();
+        private LinkedList<Contact> honorsNorthW = new LinkedList<Contact>();
+        private LinkedList<Contact> honorsSouthM = new LinkedList<Contact>();
+        private LinkedList<Contact> honorsSouthW = new LinkedList<Contact>();
+        private LinkedList<Contact> mcCutcheonM = new LinkedList<Contact>();
+        private LinkedList<Contact> mcCutcheonW = new LinkedList<Contact>();
+        private LinkedList<Contact> meredithNE = new LinkedList<Contact>();
+        private LinkedList<Contact> meredithNW = new LinkedList<Contact>();
+        private LinkedList<Contact> meredithSE = new LinkedList<Contact>();
+        private LinkedList<Contact> meredithSW = new LinkedList<Contact>();
+        private LinkedList<Contact> owenM = new LinkedList<Contact>();
+        private LinkedList<Contact> owenW = new LinkedList<Contact>();
+        private LinkedList<Contact> shreveM = new LinkedList<Contact>();
+        private LinkedList<Contact> shreveW = new LinkedList<Contact>();
+        private LinkedList<Contact> tark = new LinkedList<Contact>();
+        private LinkedList<Contact> thirdStreetM = new LinkedList<Contact>();
+        private LinkedList<Contact> thirdStreetW = new LinkedList<Contact>();
+        private LinkedList<Contact> wileyM = new LinkedList<Contact>();
+        private LinkedList<Contact> wileyW = new LinkedList<Contact>();
+        private LinkedList<Contact> windsorDuhme = new LinkedList<Contact>();
+        private LinkedList<Contact> windsorShealy = new LinkedList<Contact>();
+        private LinkedList<Contact> windsorVawter = new LinkedList<Contact>();
+        private LinkedList<Contact> windsorWalter = new LinkedList<Contact>();
+        private LinkedList<Contact> windsorWood = new LinkedList<Contact>();
+        #endregion
+
+        public Sorter()
+        {
+
+        }
+    }
+}
